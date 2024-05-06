bab8b343e27ae8ff39864c18f5cd36deafeaab85
TedHub2003 <123023063+TedHub2003@users.noreply.github.com>
2024-05-05 22:56:01 -0400

    Initialisatio_commit

---
+++ src/test/java/org/example/premierbears/PremierBearsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.premierbears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierBearsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
